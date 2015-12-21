a308c1458fe6d44ccb3d47e8d568b84b8bacb554
kennedy <kennedysgarage@gmail.com>
2015-12-20 19:11:05 -0500

    updating screenshot

--- functions.php
+++ functions.php
@@ -103,14 +103,6 @@ function html5blank_header_scripts()
     }
 }
 
-// Load HTML5 Blank conditional scripts
-// function html5blank_conditional_scripts()
-// {
-//     if (is_page('pagenamehere')) {
-//         wp_register_script('scriptname', get_template_directory_uri() . '/js/scriptname.js', array('jquery'), '1.0.0'); // Conditional script(s)
-//         wp_enqueue_script('scriptname'); // Enqueue it!
-//     }
-// }
 
 // Load HTML5 Blank styles
 function html5blank_styles()
